b216e6c32c3fbabf092c27b5c3426d6c5024cd6e
Oleg Kovalenko <monstrenyatko@gmail.com>
2017-10-06 22:47:34 -0500

    Fix `admin` URL.

--- butler_api/urls.py
+++ butler_api/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', include('admin.site.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^auth/', include('auth_manager.urls')),
     url(r'^fw/', include('fw_manager.urls')),
     url(r'^cert/', include('cert_manager.urls')),
