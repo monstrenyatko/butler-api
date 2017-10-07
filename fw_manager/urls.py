f2b22e5356137ffa2594e8457dfbaa825189db1b
Oleg Kovalenko <monstrenyatko@gmail.com>
2017-10-07 00:36:27 -0500

    Remove unnecessary escape symbols from URLs.

--- fw_manager/urls.py
+++ fw_manager/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views as local_view
 
 urlpatterns = [
-    url(r'^firmwares\/$', local_view.FirmwareListView.as_view()),
-    url(r'^firmwares\/(?P<name>[^\s\/]+)\/$', local_view.FirmwareDetailView.as_view()),
-    url(r'^assignments\/$', local_view.FirmwareAssignmentListView.as_view()),
-    url(r'^assignments\/(?P<username>[^\s\/]+)\/$', local_view.FirmwareAssignmentDetailView.as_view()),
-    url(r'^upload\/(?P<filename>[^\s\/]+)\/$', local_view.FirmwareUploadView.as_view()),
-    url(r'^update\/$', local_view.FirmwareUpdateView.as_view()),
-    url(r'^update\/(?P<username>[^\s\/]+)\/$', local_view.FirmwareUpdateAnonymousView.as_view()),
+    url(r'^firmwares/$', local_view.FirmwareListView.as_view()),
+    url(r'^firmwares/(?P<name>[^\s\/]+)/$', local_view.FirmwareDetailView.as_view()),
+    url(r'^assignments/$', local_view.FirmwareAssignmentListView.as_view()),
+    url(r'^assignments/(?P<username>[^\s\/]+)/$', local_view.FirmwareAssignmentDetailView.as_view()),
+    url(r'^upload/(?P<filename>[^\s\/]+)/$', local_view.FirmwareUploadView.as_view()),
+    url(r'^update/$', local_view.FirmwareUpdateView.as_view()),
+    url(r'^update/(?P<username>[^\s\/]+)/$', local_view.FirmwareUpdateAnonymousView.as_view()),
 ]
